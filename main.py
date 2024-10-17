c48b675e6875842bc5c4ffaf5efc6c180e67bec0
Shehryar1021 <kiranimtiaz1985@gmail.com>
2024-10-17 16:13:59 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+string = input("PLEASE ENTER YOUR OWN STRING :")
+string2 = ('')
+for i in string:
+    string2= i +string2
+    print("\nTHE ORIGINAL STRING = ",string)
+    print("THE REVERSED STRING = ",string2)
